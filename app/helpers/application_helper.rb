87c00854bb58a5e625189e673e6f367ef5998092
Viet Pham <pxviet1997@gmail.com>
2025-01-03 11:42:52 +0700

    update header ui

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
     options[:title] ||= name.underscore.humanize
     options[:aria] = true
     options[:nocomment] = true
-    options[:class] = options.fetch(:styles, 'fill-current text-gray-500')
+    options[:class] = options.fetch(:styles, 'fill-current text-gray-500 size-6')
     filename = "#{name}.svg"
     inline_svg_tag(filename, options)
   end
